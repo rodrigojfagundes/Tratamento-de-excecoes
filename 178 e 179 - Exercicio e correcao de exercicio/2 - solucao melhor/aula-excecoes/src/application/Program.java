dbe6efb20eac78494092ddc8414f2876ec152df8
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-03-05 19:10:27 -0300

    Exercicio e correção de exercicio
    Aulas 178 e 179.

---
+++ 178 e 179 - Exercicio e correcao de exercicio/2 - solucao melhor/aula-excecoes/src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("informe os dados da conta");
+		System.out.println("numero");
+		int number = sc.nextInt();
+		System.out.println("titular");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.println("saldo inicial");
+		double balance = sc.nextDouble();
+		System.out.println("limite de saque");
+		double withdrawLimit = sc.nextDouble();
+		
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.println("informe a quantia para sacar");
+		
+		double amount = sc.nextDouble();
+	
+		String error = acc.validateWithdraw(amount);
+
+		if (error != null) {
+			System.out.println(error);
+		}
+		
+		else {
+		acc.withdraw(amount);
+		System.out.printf("novo saldo: %.2f%n", acc.getBalance());
+		}
+	}
+}
---
+++ 178 e 179 - Exercicio e correcao de exercicio/3 - a melhor solucao/aula-excecoes/src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+import exceptions.BusinessException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("informe os dados da conta");
+		System.out.println("numero");
+		int number = sc.nextInt();
+		System.out.println("titular");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.println("saldo inicial");
+		double balance = sc.nextDouble();
+		System.out.println("limite de saque");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.println("informe a quantia para sacar");
+		
+		double amount = sc.nextDouble();
+
+		try {
+		acc.withdraw(amount);
+		System.out.printf("novo saldo: %.2f%n", acc.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
